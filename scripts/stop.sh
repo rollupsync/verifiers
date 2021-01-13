f8114cf5ce5b892fc2715a42cda321d4463d2d9a
Chance Hudson <jchancehud@gmail.com>
2021-01-12 19:02:12 -0600

    fix string comparison

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ -z $1 -o $1 == 'help' -o $1 == '--help' ]
+if [ -z $1 -o $1 = 'help' -o $1 = '--help' ]
 then
   echo "Usage: npm run stop <mainnet|rinkeby>"
   echo ""
   exit
 fi
 
-if [ $1 == 'mainnet' -o $1 == 'rinkeby' ]
+if [ $1 = 'mainnet' -o $1 = 'rinkeby' ]
 then
   exec docker-compose -f ./configs/docker-compose.$1.yaml -p ${1}_verifiers down
 else
